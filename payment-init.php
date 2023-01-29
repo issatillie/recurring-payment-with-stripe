553e742fe975833dfbf5807818c205ee9cf32290
issatillie <101006467+issatillie@users.noreply.github.com>
2023-01-29 21:54:37 +0100

    Update payment-init.php

--- payment-init.php
+++ payment-init.php
@@ -1,39 +1,42 @@
 <?php
-// 1. include the Stripe PHP library
+
+// Load Stripe library
 require_once 'stripe-php/init.php';
 
-// 2. Set your secret key
-\Stripe\Stripe::setApiKey("sk_test_your_secret_key");
+// Set your secret key
+\Stripe\Stripe::setApiKey("sk_test_your_stripe_secret_key");
 
-// 3. Get the customer and subscription id from the client-side
-$customer_id = $_POST['customer_id'];
-$plan_id = $_POST['plan_id'];
+// Get customer information
+$customer_email = $_POST['email'];
+$token = $_POST['stripeToken'];
 
-try {
-  // 4. Create a new subscription for the customer
-  $subscription = \Stripe\Subscription::create([
-    'customer' => $customer_id,
-    'items' => [['plan' => $plan_id]],
-    'expand' => ['latest_invoice.payment_intent']
-  ]);
+// Create a customer
+$customer = \Stripe\Customer::create(array(
+  "email" => $customer_email,
+  "source" => $token,
+));
 
-  // 5. Check if the payment was successful
-  if ($subscription->status == 'active') {
-    // 6. Connect to the database and update the customer's information
-    $conn = new mysqli("host", "user", "password", "database");
-    $sql = "UPDATE customers SET subscription_id='".$subscription->id."', subscription_status='active' WHERE customer_id='".$customer_id."'";
-    $conn->query($sql);
-    $conn->close();
-    
-    // 7. Return success message to the client
-    echo json_encode(["message" => "Subscription created and database updated successfully."]);
-  } else {
-    // 8. Return error message to the client
-    echo json_encode(["error" => "Payment failed. Please try again later."]);
-  }
-} catch (Exception $e) {
-  // 9. Return error message to the client
-  echo json_encode(["error" => $e->getMessage()]);
+// Create a subscription for the customer
+$subscription = \Stripe\Subscription::create(array(
+  "customer" => $customer->id,
+  "items" => array(
+    array(
+      "plan" => "your_plan_id",
+    ),
+  ),
+));
+
+// If subscription is successfully created, update database table
+if ($subscription->status == "active") {
+  // Connect to database
+  $conn = new mysqli("host", "username", "password", "dbname");
+
+  // Update customer information in database table
+  $sql = "UPDATE customers SET stripe_customer_id = '" . $customer->id . "', subscription_id = '" . $subscription->id . "' WHERE email = '" . $customer_email . "'";
+  $conn->query($sql);
+
+  // Close database connection
+  $conn->close();
 }
 
 ?>
